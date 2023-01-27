b65eed3447f590fea8bfee1431bd2af37f2412ac
HyejinUm <uhj1993@naver.com>
2023-01-27 22:36:19 +0900

    ✨ feat(custom): feat addon
    ISSUES CLOSED: #1

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,16 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: [
+    '@storybook/addon-docs',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@storybook/addon-postcss',
     '@storybook/addon-interactions',
   ],
   framework: '@storybook/react',
   features: {
+    buildStoriesJson: true,
     interactionsDebugger: true,
   },
 };
