a8830091204da553dcd31604ea47c4d41360e9a0
HyejinUm <uhj1993@naver.com>
2023-01-28 15:36:20 +0900

    ✨ feat(custom): feat storybook logo

---
+++ .storybook/theme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming';
+import Logo from './logo.png';
+
+export default create({
+  base: 'light',
+  brandTitle: 'uhgenie-ui storybook',
+  brandUrl: 'https://uhgenie7.github.io/',
+  brandImage: Logo,
+  brandTarget: '_self',
+});
